02324c82788a39197089f5c2c4313f6c00ce5e0d
adblackwell4 <adblackwell4@live.com>
2026-09-11 00:41:04 -0400

    Delete Locentrix directory

--- Locentrix/js/parallax.js
+++
@@ -1,6 +0,0 @@
-let mouseX = 0, mouseY = 0;
-
-document.addEventListener('mousemove', (e) => {
-    mouseX = (e.clientX / window.innerWidth - 0.5) * 2;
-    mouseY = (e.clientY / window.innerHeight - 0.5) * 2;
-});
