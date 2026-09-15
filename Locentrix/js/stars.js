d67d3e15356d69017cd2f27cb44212bcf102e6ca
adblackwell4 <adblackwell4@live.com>
2026-09-14 22:16:36 -0400

    Staging for production site

---
+++ Locentrix/js/stars.js
@@ -0,0 +1,18 @@
+const starGeometry = new THREE.BufferGeometry();
+const starCount = 3000;
+const starPositions = new Float32Array(starCount * 3);
+
+for (let i = 0; i < starCount * 3; i++) {
+    starPositions[i] = (Math.random() - 0.5) * 300;
+}
+
+starGeometry.setAttribute('position', new THREE.BufferAttribute(starPositions, 3));
+
+const starMaterial = new THREE.PointsMaterial({
+    color: 0xffffff,
+    size: 0.8,
+    transparent: true
+});
+
+const stars = new THREE.Points(starGeometry, starMaterial);
+scene.add(stars);
